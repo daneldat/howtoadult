46fa7c0d43fd2e11cd47b51ff891a6023df48530
daneldat <56526214+daneldat@users.noreply.github.com>
2019-11-23 01:04:06 -0500

    Fixed the quote component on the page.js and on the homescreen.js

--- how-to-adult/src/Components/Pages/HomeScreen.js
+++ how-to-adult/src/Components/Pages/HomeScreen.js
@@ -8,29 +8,9 @@ import {
   useParams
 } from "react-router-dom";
 import "../../w3.css";
-import RandomQuotes from './RandomQuotes';
 import List from './List';
 import PageList from "./PageList";
-
-export default class Homepage extends React.Component {
-  render() {
-    return (
-      <div className='w3-container w3-blue-grey' style={{ height: "100vh" }}>
-        <div className='w3-container w3-bar-block w3-center' style={{ "margin-left": "15%","margin-top":"15%" }}>
-          <h2 className="w3-xxxlarge">How To Adult</h2>
-          <h3>A helpful guide to how to do adult human things.</h3>
-          <p className=" w3-container w3-center" style={{ "margin-top":"10%" }}>
-             Are you an adult who doesn't know how to adult? You've made it to the right place!
-             As fellow pseudo-adults, our researchers have scoured far and wide to identify key problems that we face in our daily lives, and consulted some adultier adults to give us some answers.
-             Just click on one of the links on the sidebar to learn how to adult, and if you have any questions or ideas to add to our database of adulting tips, let us know down below
-       </p>
-       <QuoteBank></QuoteBank>
-       <List items={this.state.workingList} />
-        </div>
-      </div>
-    )
-  }
-}
+import RandomQuotes from './RandomQuotes';
 
 class Quote extends Component {
   render() {
@@ -59,14 +39,35 @@ class QuoteBank extends Component {
   }
 }
 
-class Home extends React.Component {
+
+export default class HomeScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       list: [],
       workingList: []
     }
-    this.state.list= PageList.map((Page, index) => (<li><Link to={"/page/" + Page.props.title.replace(/\s/g, "") + "-pageid" + index} className="w3-bar-item w3-button w3-grey"> {Page.props.title}</Link></li>))
+    this.state.list= 
+    PageList.map((Page, index) => 
+    (<ul><Link to={"/page/" + Page.props.title.replace(/\s/g, "") + "-pageid" + index} className="w3-bar-item w3-button w3-bue-grey"> {Page.props.title}</Link></ul>))
     this.state.workingList = this.state.list.map(link => ({link, text: link.props.children.props.children[1]}))
   }
+  render() {
+    return (
+      <div className='w3-container w3-blue-grey' style={{ "height": "100vh", "overflow":"auto"}}>
+        <div className='w3-container w3-bar-block w3-center' style={{ "margin-left": "15%","margin-top":"15%" }}>
+          <h2 className="w3-xxxlarge">How To Adult</h2>
+          <h3>A helpful guide to how to do adult human things.</h3>
+          <p className=" w3-container w3-center" style={{ "margin-top":"10%" }}>
+             Are you an adult who doesn't know how to adult? You've made it to the right place!
+             As fellow pseudo-adults, our researchers have scoured far and wide to identify key problems that we face in our daily lives, and consulted some adultier adults to give us some answers.
+             Just click on one of the links on the sidebar to learn how to adult, and if you have any questions or ideas to add to our database of adulting tips, let us know down below
+       </p>
+       <QuoteBank/>
+       <List items={this.state.workingList} />
+        </div>
+      </div>
+    )
+  }
 }
+
