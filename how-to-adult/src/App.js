399160e3ea4b4c569603d403ec30c88975625f7e
cuhlone <56360169+cuhlone@users.noreply.github.com>
2019-11-25 12:22:10 -0500

    this has search on side bar
    but has an error

--- how-to-adult/src/App.js
+++ how-to-adult/src/App.js
@@ -11,39 +11,42 @@ import PageList from "./Components/Pages/PageList";
 import Home from "../src/Components/Pages/HomeScreen";
 import List from "./Components/Pages/List";
 
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [],
+      workingList: []
+    }
+    this.state.list =
+      PageList.map((Page, index) =>
+        (<Link to={"/page/" + Page.props.title.replace(/\s/g, "") + "-pageid" + index} className="w3-bar-item w3-button w3-bue-grey"> {Page.props.title}</Link>))
+    this.state.workingList = this.state.list.map(link => ({ link, text: link.props.children[1] }))
+  }
 
-export default () => {
-  return (
-
-    <Router>
-      <div className="w3-sidebar w3-bar-block w3-blue-grey" style={{ "width": "15%" }}>
-        <Link to="/" className="w3-bar-item w3-button w3-blue-grey">Home</Link>
-        {PageList.map((Page, index) => (<Link to={"/page/" + Page.props.title.replace(/\s/g, "") + "-pageid" + index} className="w3-bar-item w3-button w3-blue-grey"> {Page.props.title}</Link>))}
-
-      </div>
-
-      <Switch>
-        <Route path="/page/:id" children={<Child />} />
-        <Route path="/" component={Home}/>
-      </Switch>
-    </Router>
-  );
-}
-function Child() {
-  // We can use the `useParams` hook here to access
-  // the dynamic pieces of the URL.
-  let { id } = useParams();
+  child() {
+    let { id } = useParams();
   id = id.split("-pageid")[1]
 
   return (
-PageList[id]
+    PageList[id]
   );
-}
+  }
 
-{/* <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route> */}
-{}
+  render() {
+    return (
+
+      <Router>
+        <div className="w3-sidebar w3-bar-block w3-blue-grey" style={{ "width": "15%" }}>
+          <Link to="/" className="w3-bar-item w3-button w3-blue-grey">Home</Link>
+          <List items={this.state.workingList} />
+        </div>
+
+        <Switch>
+          <Route path="/page/:id" children={this.child} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
+    );
+  }
+}
