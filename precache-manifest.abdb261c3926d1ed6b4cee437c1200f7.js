8333d61b197dfce4c80aa7f1ebab81f69507b453
daneldat <56526214+daneldat@users.noreply.github.com>
2019-12-04 11:47:42 -0500

    Updates

---
+++ precache-manifest.abdb261c3926d1ed6b4cee437c1200f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eab8000f3f8cb5fef1d7ee43ff9c5ba0",
+    "url": "/howtoadult/index.html"
+  },
+  {
+    "revision": "b99c0851c2b230dd799a",
+    "url": "/howtoadult/static/css/main.defc26f5.chunk.css"
+  },
+  {
+    "revision": "bac1db614fb046c4ea55",
+    "url": "/howtoadult/static/js/2.18eecb00.chunk.js"
+  },
+  {
+    "revision": "b99c0851c2b230dd799a",
+    "url": "/howtoadult/static/js/main.c337df0c.chunk.js"
+  },
+  {
+    "revision": "54463c3a945d108de7be",
+    "url": "/howtoadult/static/js/runtime-main.3cc4e864.js"
+  }
+]);
